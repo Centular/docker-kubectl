5e5e819781a237e3f59838aa9a8aa0b6e7a31c5d
Wessel Pieterse <wessel@ordercloud.co.za>
2017-05-02 12:20:33 +0200

    Fixed the replace on server name to ignore the /

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -i 's/\$CLUSTER_NAME/'"$CLUSTER_NAME"'/g' /config/.kube/config
-sed -i 's/\$CLUSTER_SERVER/'"$CLUSTER_SERVER"'/g' /config/.kube/config
+sed -i 's#\$CLUSTER_SERVER#'"$CLUSTER_SERVER"'#g' /config/.kube/config
 sed -i 's/\$CLUSTER_CA/'"$CLUSTER_CA"'/g' /config/.kube/config
 sed -i 's/\$CLUSTER_USER/'"$CLUSTER_USER"'/g' /config/.kube/config
 sed -i 's/\$CLUSTER_TOKEN/'"$CLUSTER_TOKEN"'/g' /config/.kube/config
